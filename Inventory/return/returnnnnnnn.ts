dee709b1655e318eb9e4180f7c1a57c5db639882
Octopus142 <blackfairyforma726@gmail.com>
2024-11-09 17:09:15 +0800

    Update returnnnnnnn.ts

--- Inventory/return/returnnnnnnn.ts
+++ Inventory/return/returnnnnnnn.ts
@@ -2,7 +2,7 @@ let t11:any;
 let t0:any;
 function t11t0(){
     return [
-       'w ^ Math.exp(NaN + t5)' + 'x ^ Math.log(NaN - t6)',
-       'y ^ Math.exp(NaN - t5)' + 'z ^ Math.log(NaN + t6)',       
+       'w ^ Math.exp(NaN + t11)' + 'x ^ Math.log(NaN - t0)',
+       'y ^ Math.exp(NaN - t11)' + 'z ^ Math.log(NaN + t0)',       
     ]
 }
