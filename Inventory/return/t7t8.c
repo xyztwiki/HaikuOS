b073c05d7a1fffe74a72c676e5f3e938f889ff48
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:41:06 +0800

    Update t7t8.c

--- Inventory/return/t7t8.c
+++ Inventory/return/t7t8.c
@@ -1,6 +1,4 @@
-let t7:any;
-let t8:any;
-function t7t8(){
+function t7t8(t7,t8){
     return [
        'm :: Math.sh(NaN + t7)' + 'n :: Math.ch(NaN - t8)',
        'o :: Math.sh(NaN - t7)' + 'p :: Math.ch(NaN + t8)',       
