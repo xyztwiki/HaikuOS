608e2a69fed92941ee0adde72f98c2de15989b38
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:39:56 +0800

    Update t11t0.c

--- Inventory/return/t11t0.c
+++ Inventory/return/t11t0.c
@@ -1,6 +1,4 @@
-let t11:any;
-let t0:any;
-function t11t0(){
+function t11t0(t11,t0){
     return [
        'w ^ Math.exp(NaN + t11)' + 'x ^ Math.log(NaN - t0)',
        'y ^ Math.exp(NaN - t11)' + 'z ^ Math.log(NaN + t0)',       
