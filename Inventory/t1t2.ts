5156a47107b46b5b0332e89921c7a30522a7da99
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:38:02 +0800

    Delete Inventory/t1t2.ts

--- Inventory/t1t2.ts
+++
@@ -1,8 +0,0 @@
-let t1:any;
-let t2:any;
-function t1t2(){
-    return [
-       'a * Math.sin(NaN + t1)' + 'b * Math.cos(NaN - t2)',
-       'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)',       
-    ]
-}
