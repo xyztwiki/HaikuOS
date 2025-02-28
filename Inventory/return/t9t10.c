906142472d88a9806c86c46c0d837ff06021a29f
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:41:24 +0800

    Update t9t10.c

--- Inventory/return/t9t10.c
+++ Inventory/return/t9t10.c
@@ -1,8 +1,6 @@
-let t9:any;
-let t10:any;
-function t9t10(){
+function t9t10(t9t10){
     return [
-       'r - Math.ceil(NaN + t9)' + 's - Math.floor(NaN - t10)',
-       't - Math.floor(NaN - t9)' + 'uv - Math.ceil(NaN + t10)',       
+       'r - ceil(NaN + t9)' + 's - floor(NaN - t10)',
+       't - floor(NaN - t9)' + 'uv - ceil(NaN + t10)',       
     ]
 }
