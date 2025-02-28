d99d76573dd926f0df42f4876482d06152a40326
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:40:21 +0800

    Update t1t2.c

--- Inventory/return/t1t2.c
+++ Inventory/return/t1t2.c
@@ -1,8 +1,6 @@
-let t1:any;
-let t2:any;
 function t1t2(){
     return [
-       'a * Math.sin(NaN + t1)' + 'b * Math.cos(NaN - t2)',
-       'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)',       
+       'a * sin(NaN + t1)' + 'b * cos(NaN - t2)',
+       'c * sin(NaN - t1)' + 'd * cos(NaN + t2)',       
     ]
 }
