78ea82e058fa9c62c71d100cb37795e063cdb857
xyztwiki <blackfairyforma726@gmail.com>
2025-08-11 09:16:17 +0800

    Delete CHNO directory

--- CHNO/C/RNA.c
+++
@@ -1,8 +0,0 @@
-#include "pentagon.c"
-#include "../element/element.vb"
-void RNA(){
-   "A : a / b \ c \ d _ e /"
-   "C : f / g \ i \ j _ k /" 
-   "G : l / m \ n \ o _ p /"
-   "U : x | y | z"
-;}
--- CHNO/element/Emc.ts
+++
@@ -1,4 +0,0 @@
-function Core(mass:any){
-    const c = 299792458
-    const E = mass * c * c
-}
