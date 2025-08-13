c13f9130231fcbcd51b6f32e3d46e5c233e57356
xyztwiki <blackfairyforma726@gmail.com>
2025-08-13 09:40:08 +0800

    Update number.ts

--- Symbol/number.ts
+++ Symbol/number.ts
@@ -1,12 +1,12 @@
 function number(){
-    const one:number = 1
-    const two:number = 2
-    const three:number = 3  
-    const four:number = 4
-    const five:number = 5
-    const six:number = 6
-    const seven:number = 7
-    const eight:number = 8
-    const nine:number = 9
-    const zero:number = 0
+    one:number = 1
+    two:number = 2
+    three:number = 3  
+    four:number = 4
+    five:number = 5
+    six:number = 6
+    seven:number = 7
+    eight:number = 8
+    nine:number = 9
+    zero:number = 0
 }
