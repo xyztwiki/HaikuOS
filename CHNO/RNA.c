1f0bae45371a0670220c69244cefe66c207e9873
xyztwiki <blackfairyforma726@gmail.com>
2025-08-13 09:38:01 +0800

    Update RNA.c

--- CHNO/RNA.c
+++ CHNO/RNA.c
@@ -3,10 +3,10 @@ char pentagon =
         "/ \
          \_/";
 }
-         
 void RNA(){
    "A : a / b \ c \ d _ e /"
    "C : f / g \ i \ j _ k /" 
    "G : l / m \ n \ o _ p /"
    "U : x | y | z"
 ;}
+
